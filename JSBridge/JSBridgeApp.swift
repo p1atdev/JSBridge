6c84f361bddc83d7b7867eacd80ea7861efae640
p1atdev <shu_teiei@outlook.jp>
2022-06-25 15:12:41 +0900

    Initial Commit

---
+++ JSBridge/JSBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSBridgeApp.swift
+//  JSBridge
+//
+//  Created by p1atdev on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct JSBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
