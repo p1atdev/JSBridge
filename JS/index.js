c76e2903ede8406ccf65a9fad69d433ab96d3729
p1atdev <shu_teiei@outlook.jp>
2022-07-03 11:34:59 +0900

    chore: simpler

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,7 @@
 import * as lodash from "lodash"
 
 export class Bridge {
+    // 配列の和を取る
     static sum(array) {
         // インジェクトされているか検証
         if (typeof sendMessage === "function") {
@@ -9,5 +10,14 @@ export class Bridge {
         }
 
         return lodash.sum(array)
+    }
+
+    // 時間のかかる処理
+    static async slowProcess() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("Done!")
+            }, 1000)
+        })
     }
 }
